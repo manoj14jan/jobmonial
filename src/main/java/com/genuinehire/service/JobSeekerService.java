7a262259e12f9cad87bcfac107bbac87672d43f2
Bleard <rexhajbleard@gmail.com>
2020-06-18 03:51:50 -0500

    Job Seeker Service Divided into implementation and service.

--- src/main/java/com/genuinehire/service/JobSeekerService.java
+++ src/main/java/com/genuinehire/service/JobSeekerService.java
@@ -1,35 +1,13 @@
 package com.genuinehire.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import com.genuinehire.domain.JobSeeker;
-import com.genuinehire.repository.JobSeekerRepository;
 
 import java.util.List;
 
-@Service
-public class JobSeekerService {
+public interface JobSeekerService {
 
-	@Autowired
-	private JobSeekerRepository jobSeekerRepository;
-
-	public JobSeeker saveJobSeeker(JobSeeker jobSeeker) {
-		if(jobSeeker.getStatus() == null || jobSeeker.getStatus().equalsIgnoreCase("")) {
-			jobSeeker.setStatus("seeking");
-		}
-		return jobSeekerRepository.save(jobSeeker);
-	}
-	
-	public JobSeeker getJobSeekerByUsername(String username){
-		return jobSeekerRepository.findByUserUsername(username);
-	}
-
-    public List<JobSeeker> getAllJobSeekers() {
-		return (List<JobSeeker>)jobSeekerRepository.findAll();
-    }
-
-    public JobSeeker getJobSeekerById(Long id) {
-		return jobSeekerRepository.findOne(id);
-    }
+	JobSeeker saveJobSeeker(JobSeeker jobSeeker);
+	JobSeeker getJobSeekerByUsername(String username);
+    List<JobSeeker> getAllJobSeekers();
+    JobSeeker getJobSeekerById(Long id);
 }
---
+++ src/main/java/com/genuinehire/service/impl/JobSeekerServiceImpl.java
@@ -0,0 +1,36 @@
+package com.genuinehire.service.impl;
+
+import com.genuinehire.service.JobSeekerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.genuinehire.domain.JobSeeker;
+import com.genuinehire.repository.JobSeekerRepository;
+
+import java.util.List;
+
+@Service
+public class JobSeekerServiceImpl implements JobSeekerService {
+
+    @Autowired
+    private JobSeekerRepository jobSeekerRepository;
+
+    public JobSeeker saveJobSeeker(JobSeeker jobSeeker) {
+        if(jobSeeker.getStatus() == null || jobSeeker.getStatus().equalsIgnoreCase("")) {
+            jobSeeker.setStatus("seeking");
+        }
+        return jobSeekerRepository.save(jobSeeker);
+    }
+
+    public JobSeeker getJobSeekerByUsername(String username){
+        return jobSeekerRepository.findByUserUsername(username);
+    }
+
+    public List<JobSeeker> getAllJobSeekers() {
+        return (List<JobSeeker>)jobSeekerRepository.findAll();
+    }
+
+    public JobSeeker getJobSeekerById(Long id) {
+        return jobSeekerRepository.findOne(id);
+    }
+}
